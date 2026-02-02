ea510fb1a3d7d651b4af3226deeb9ade4942ead2
George Jeremy <ksacck@gmail.com>
2026-02-02 19:00:41 +0700

    empty service

---
+++ src/main/java/test/task/normalization/service/PhoneNormalizationService.java
@@ -0,0 +1,11 @@
+package test.task.normalization.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PhoneNormalizationService {
+
+    public String normalize(String rawPhone) {
+        return null;
+    }
+}
