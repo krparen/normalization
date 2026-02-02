8b920a28def7da15791383e6f39a17e8a73b69fa
George Jeremy <ksacck@gmail.com>
2026-02-02 18:53:26 +0700

    first commit

---
+++ src/main/java/test/task/normalization/NormalizationApplication.java
@@ -0,0 +1,13 @@
+package test.task.normalization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormalizationApplication.class, args);
+    }
+
+}
