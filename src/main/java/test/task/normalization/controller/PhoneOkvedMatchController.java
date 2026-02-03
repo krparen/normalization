82b378f9fa607c950fddaa863dbe1aa70f934e9e
George Jeremy <ksacck@gmail.com>
2026-02-03 13:36:43 +0700

    переделал входной запрос на POST

--- src/main/java/test/task/normalization/controller/PhoneOkvedMatchController.java
+++ src/main/java/test/task/normalization/controller/PhoneOkvedMatchController.java
@@ -1,10 +1,11 @@
 package test.task.normalization.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import test.task.normalization.dto.MatchPhoneOkvedResultDto;
+import test.task.normalization.dto.PhoneOkvedRequestDto;
 import test.task.normalization.service.MatchPhoneOkvedService;
 
 @RestController
@@ -13,8 +14,8 @@ public class PhoneOkvedMatchController {
 
     private final MatchPhoneOkvedService matchPhoneOkvedService;
 
-    @GetMapping("/match")
-    public MatchPhoneOkvedResultDto match(@RequestParam String phone) {
-        return matchPhoneOkvedService.matchPhoneToOkveds(phone);
+    @PostMapping("/match")
+    public MatchPhoneOkvedResultDto match(@RequestBody PhoneOkvedRequestDto request) {
+        return matchPhoneOkvedService.matchPhoneToOkveds(request.getPhone());
     }
 }
---
+++ src/main/java/test/task/normalization/dto/PhoneOkvedRequestDto.java
@@ -0,0 +1,8 @@
+package test.task.normalization.dto;
+
+import lombok.Data;
+
+@Data
+public class PhoneOkvedRequestDto {
+    private String phone;
+}
