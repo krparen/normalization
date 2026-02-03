b56ba83c6ffc6fbbca6c087aab03310ccbb62b35
George Jeremy <ksacck@gmail.com>
2026-02-03 14:44:26 +0700

    ещё тест MatchPhoneService

--- src/test/java/test/task/normalization/service/MatchPhoneOkvedServiceTest.java
+++ src/test/java/test/task/normalization/service/MatchPhoneOkvedServiceTest.java
@@ -1,5 +1,6 @@
 package test.task.normalization.service;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -26,23 +27,43 @@ class MatchPhoneOkvedServiceTest {
     @InjectMocks
     private MatchPhoneOkvedService matchPhoneOkvedService;
 
-    @Test
-    void matchPhoneToOkveds() {
-        List<FlatOkvedDto> okveds = new ArrayList<>();
+    private static FlatOkvedDto childOkved;
+    private static FlatOkvedDto parentOkved;
 
-        FlatOkvedDto childOkved = new FlatOkvedDto();
+    @BeforeAll
+    public static void init(){
+        childOkved = new FlatOkvedDto();
         childOkved.setCode("88.8");
         childOkved.setCodeNoDots("888");
         childOkved.setName("Три восьмёрки");
 
-        FlatOkvedDto parentOkved = new FlatOkvedDto();
+        parentOkved = new FlatOkvedDto();
         parentOkved.setCode("88");
         parentOkved.setCodeNoDots("88");
         parentOkved.setName("Восемь восемь");
+    }
 
+    @Test
+    void matchPhoneToOkveds_longerMatch() {
+        List<FlatOkvedDto> okveds = List.of(parentOkved, childOkved);
+        String phone = "79995554888";
+        String normalizedPhone = "+" + phone;
+
+        when(phoneNormalizationService.normalize(phone)).thenReturn(normalizedPhone);
+        when(okvedRegistryClient.getOkvedsFromGithub()).thenReturn(okveds);
+
+        MatchPhoneOkvedResultDto result = matchPhoneOkvedService.matchPhoneToOkveds(phone);
+
+        assertEquals(3, result.getMatchLength());
+        assertEquals(childOkved.getCode(), result.getShortOkvedDto().getCode());
+        assertEquals(childOkved.getName(), result.getShortOkvedDto().getName());
+        assertEquals(normalizedPhone, result.getNormalizedPhone());
+    }
+
+    @Test
+    void matchPhoneToOkveds_fullMatchAfterNotFullMatch() {
         // порядок важен - проверяем кейс, когда "полный" мэтч должен заместить в результате неполный
-        okveds.add(childOkved);
-        okveds.add(parentOkved);
+        List<FlatOkvedDto> okveds = List.of(childOkved, parentOkved);
 
         String phone = "79995554488";
         String normalizedPhone = "+" + phone;
